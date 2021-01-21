244d51e992441d703b71e3e3116aca5b6d7cc088
JACKSON GERBES <jg0244@students.necsd.net>
2021-01-21 00:01:00 +0000

    prob 1 3 Jackson Gerbes

--- Prob2.java
+++ Prob2.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 class Prob2 {
   public static void main(String[] args) {
-    
+
+    Scannner scan = new Scanner(System.in);
+    System.out.print("Enter a string");
+    String word = scan.nextLine();
+    System.out.println(word.substring(0,1));
+    System.out.println(word.
+
+
+
+
   }
 }
--- Prob3.java
+++ Prob3.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 
+import com.sun.org.apache.xerces.internal.xni.QName;
+
 class Prob3 {
   public static void main(String[] args) {
-    
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter another word: ");
+
+  String word = scan.nextLine();
+  word = word.toLowerCase();
+
+  System.out.println("Enter another word: ");
+  String word2 = scan.nextLine();
+  word2 = word2.toLowerCase();
+
+  System.out.println(word.compareTo(word2));
   }
 }
